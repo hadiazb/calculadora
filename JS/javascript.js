b008b0be96ab51fb6c1a7cefb4eb284036370c65
hadiazb <hadiazb@unal.edu.co>
2020-01-08 18:13:35 -0500

    first commit

---
+++ JS/javascript.js
@@ -0,0 +1,97 @@
+function sumar()
+{
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+
+    if (validar(num1, num2))
+    {
+    var res = num1 + num2;
+    alert("la suma es: " + res);
+  }else
+  {
+    alert("Formato incorrecto!!!");
+  }
+}
+
+function restar()
+{
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+
+    if (validar(num1, num2))
+    {
+    var res = num1 - num2;
+    alert("la resta es: " + res);
+  }else
+  {
+    alert("Formato incorrecto!!!");
+  }
+}
+
+function multiplicar()
+{
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+
+    if (validar(num1, num2))
+    {
+    var res = num1 * num2;
+    alert("la multiplicacion es: " + res);
+  }else
+  {
+    alert("Formato incorrecto!!!");
+  }
+}
+
+function dividir()
+{
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+
+    if (validar(num1, num2))
+    {
+      var res = num1 / num2;
+      if (res == Infinity)
+      {
+        alert("Division entre cero");
+        document.getElementById("res".value = "");
+      }else
+      {
+          alert("La division es: " + res );
+      }
+
+    }else
+    {
+    alert("Formato incorrecto!!!");
+    }
+}
+
+function validar(num1, num2)
+{
+  if (isNaN(num1) || isNaN(num2))
+  {
+      return false;
+  }else
+  {
+      return true;
+  }
+}
+
+function hacerTabla()
+{
+  var tabla = "";
+  var num = parseFloat(document.getElementById("num").value);
+
+  if (isNaN(num) == false)
+  {
+      tabla += 'Tabla del ' + num + '<br>';
+      for (var i = 0; i <= 10; i++)
+      {
+        tabla += num + ' x ' + i + ' = ' + num * i + '<br>';
+      }
+      document.getElementById("tabla").innerHTML = tabla;
+  }else
+  {
+      alert("Formato incorrecto de numeros!!! ");
+  }
+}
